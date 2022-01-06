7f3d1eb1ce245aa389e902a1b6e88ef92e3a7337
Joey Peterson <joey.peterson15@gmail.com>
2022-01-06 09:06:24 -0800

    yee

--- ball.js
+++ ball.js
@@ -10,14 +10,15 @@ export default class Ball {
     }
 
     set x(value) {
-        this.ballElement.setProperty("--x", value)
+        this.ballElement.style.setProperty("--x", value)
     }
+    
     get y() {
         return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--y"))
     }
 
     set y(value) {
-        this.ballElement.setProperty("--y", value)
+        this.ballElement.style.setProperty("--y", value)
     }
 
     //we call this method in the script frame
--- script.js
+++ script.js
@@ -9,7 +9,7 @@ export function update (time) {
     //this just shows how time will print out the time its been since browser upload
     console.log(time)
 
-    if (lastTime !== null) {
+    if (lastTime) {
         //create a delta ==> the change in time between each function call. we can use this to increment the ball according to how the frames are being rendered.
         let delta = time - lastTime
         //pass delta into the ball update method.
